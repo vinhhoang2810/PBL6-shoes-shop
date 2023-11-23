16cd6d2efde421e821f3284b6364d28218966ba1
DNAnh01 <donguyenanhgithub@gmail.com>
2023-11-23 12:27:11 +0700

    --11-- code frontend: UI dashboard grid-template-areas

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -7,11 +7,19 @@ const cx = classNames.bind(styles);
 const Dashboard = () => {
     return (
         <Fragment>
-            <header className={`align-items-center d-flex p-4`}>
+            <header className={`align-items-center d-flex p-2`}>
                 <Icon icon="dashboard" classes={`ml-2`} />
                 <h5 className={`mb-0 ml-4`}>Dashboard</h5>
             </header>
-            <div className={cx('grid')}>fadfasdfa</div>
+            <div className={cx('grid')}>
+                <div className={cx('section1')}>section1</div>
+                <div className={cx('section2')}>section2</div>
+                <div className={cx('section3')}>section3</div>
+                <div className={cx('section4')}>section4</div>
+                <div className={cx('section5')}>section5</div>
+                <div className={cx('section6')}>section6</div>
+                <div className={cx('section7')}>section7</div>
+            </div>
         </Fragment>
     );
 };
