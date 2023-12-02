95f63b902faaeff32b5a3ad0e01e80ac82b9028a
Vanhhth <vanhth@gammapp.com>
2023-12-03 00:24:43 +0700

    fun in account

--- app/src/main/java/com/shop/shoes/project/ui/main/account/AccountFragment.kt
+++ app/src/main/java/com/shop/shoes/project/ui/main/account/AccountFragment.kt
@@ -1,20 +1,48 @@
 package com.shop.shoes.project.ui.main.account
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.airbnb.lottie.LottieDrawable
+import com.shop.shoes.project.R
 import com.shop.shoes.project.databinding.FragmentAccountBinding
+import com.shop.shoes.project.ui.auth.LoginActivity
 import com.shop.shoes.project.ui.main.base.BaseFragment
+import com.shop.shoes.project.utils.Constants
+import com.shop.shoes.project.utils.Pref
 
 class AccountFragment : BaseFragment<FragmentAccountBinding>(){
-    override fun initView() {
+    override fun initView() = binding.run{
         playAnimation()
+        if(Pref.accessToken == ""){
+            tvLogout.visibility = View.GONE
+            view.visibility = View.GONE
+        }
     }
 
     override fun initData() {
     }
 
-    override fun initListener() {
+    override fun initListener() = binding.run {
+        tvInfo.setOnClickListener { mustBeLogin {
+            startActivity(Intent(context, InfoActivity::class.java))
+        } }
+
+        tvSupport.setOnClickListener { sendFeedBack() }
+        tvLogout.setOnClickListener {
+            Pref.accessToken = ""
+            tvLogout.visibility = View.GONE
+            view.visibility = View.GONE
+        }
+        tvRate.setOnClickListener {
+            //TODO
+        }
+        tvCart.setOnClickListener {
+            //TODO
+        }
     }
 
     override fun getViewBinding(
@@ -26,6 +54,34 @@ class AccountFragment : BaseFragment<FragmentAccountBinding>(){
         binding.animationView.run {
             playAnimation()
             repeatCount = LottieDrawable.INFINITE
+        }
+    }
+
+    private fun mustBeLogin(listener: () -> Unit) {
+        if(Pref.accessToken == ""){
+            startActivityForResult(Intent(context, LoginActivity::class.java), Constants.REQUEST_CODE_LOGIN)
+        }else{
+            listener.invoke()
+        }
+    }
+
+    private fun sendFeedBack() {
+        try {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            val emailSubjectFormat = getString(R.string.we_will_support_you_as_soon_as_possible)
+            val emailSubject = String.format(emailSubjectFormat, getString(R.string.app_name)
+            )
+            emailIntent.data =
+                Uri.parse("mailto:" + getString(R.string.email_feedback))
+            emailIntent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                emailSubject
+            )
+            startActivity(Intent.createChooser(emailIntent, "Send Feedback"))
+        } catch (e: ActivityNotFoundException) {
+            toast(getString(R.string.no_mail_found))
+        } catch (e: Exception) {
+            toast(getString(R.string.error_can_not_send))
         }
     }
 
