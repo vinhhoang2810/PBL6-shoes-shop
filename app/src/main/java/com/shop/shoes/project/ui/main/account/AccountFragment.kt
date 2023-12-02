7106bdf395d11cc0cb2a875dccd23626a21fda28
Vanhhth <vanhth@gammapp.com>
2023-12-03 00:13:34 +0700

    ui account

--- app/src/main/java/com/shop/shoes/project/ui/main/account/AccountFragment.kt
+++ app/src/main/java/com/shop/shoes/project/ui/main/account/AccountFragment.kt
@@ -1,28 +1,32 @@
 package com.shop.shoes.project.ui.main.account
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.airbnb.lottie.LottieDrawable
 import com.shop.shoes.project.databinding.FragmentAccountBinding
-import com.shop.shoes.project.ui.auth.LoginActivity
 import com.shop.shoes.project.ui.main.base.BaseFragment
 
 class AccountFragment : BaseFragment<FragmentAccountBinding>(){
     override fun initView() {
+        playAnimation()
     }
 
     override fun initData() {
     }
 
     override fun initListener() {
-        binding.tvLogin.setOnClickListener{
-            startActivity(Intent(context, LoginActivity::class.java))
-        }
     }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentAccountBinding = FragmentAccountBinding.inflate(inflater)
+
+    private fun playAnimation() {
+        binding.animationView.run {
+            playAnimation()
+            repeatCount = LottieDrawable.INFINITE
+        }
+    }
 
 }
