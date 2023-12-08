a771e19b4cb478ec93040e16845e5bac21c2d75d
Vanhhth <vanhth@gammapp.com>
2023-12-08 10:57:59 +0700

    ui detail product

---
+++ app/src/main/java/com/shop/shoes/project/ui/main/detail/DetailProductActivity.kt
@@ -0,0 +1,20 @@
+package com.shop.shoes.project.ui.main.detail
+
+import android.view.LayoutInflater
+import com.shop.shoes.project.databinding.ActivityDetailProductBinding
+import com.shop.shoes.project.ui.main.base.BaseActivity
+
+class DetailProductActivity : BaseActivity<ActivityDetailProductBinding>() {
+    override fun viewBinding(inflate: LayoutInflater): ActivityDetailProductBinding =
+        ActivityDetailProductBinding.inflate(inflate)
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initListener() {
+    }
+
+}
